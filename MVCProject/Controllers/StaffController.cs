f0040e8e0c0084e4e2d07d1594303343f64dde97
quy doan duy <quy.doanduy@gmail.com>
2021-03-16 17:28:50 +0700

    create

--- MVCProject/Controllers/StaffController.cs
+++ MVCProject/Controllers/StaffController.cs
@@ -4,44 +4,83 @@ using MVCProject.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MVCProject.Controllers
 {
     public class StaffController : Controller
     {
-        public List<NhanVien> DsNhanVien = new List<NhanVien> {
-            new NhanVien("NV-0001","Đoàn Duy Quý",DateTime.Parse("28/6/2000"),"09715869331","Ha Noi","Nhan vien",5),
-            new NhanVien("NV-0002","Nguyen Van A",DateTime.Parse("3/7/1999"),"09715869331","Ha Noi","Nhan vien",5),
-            new NhanVien("NV-0003","Le Thi B",DateTime.Parse("8/12/1996"),"09715869331","Ha Noi","Nhan vien",5),
-            new NhanVien("NV-0004","Trần Văn C",DateTime.Parse("28/6/2000"),"09715869331","Ha Noi","Nhan vien",5),
-            new NhanVien("NV-0005","Bùi Tiến Đạt",DateTime.Parse("24/9/2000"),"09715869331","Ha Noi","Nhan vien",5),
-        };
-        public IActionResult Index()
+        public float LastStaffId;
+        public List<NhanVien> DsNhanVien = new List<NhanVien>();
+    public IActionResult Index()
         {
-            HttpContext.Session.SetString("dsNhanVien", JsonConvert.SerializeObject(DsNhanVien));
-
+            
+            byte[] json;
+            if (HttpContext.Session.TryGetValue("StaffList", out json)) {
+                DsNhanVien = JsonConvert.DeserializeObject<List<NhanVien>>(HttpContext.Session.GetString("StaffList"));
+                LastStaffId = JsonConvert.DeserializeObject<float>(HttpContext.Session.GetString("LastStaffId"));
+            }
+            else {
+                
+                HttpContext.Session.SetString("StaffList", JsonConvert.SerializeObject(DsNhanVien));
+                HttpContext.Session.SetString("LastStaffId", JsonConvert.SerializeObject(LastStaffId));
+            }
+            
             return View(DsNhanVien);
         }
         [HttpPost]
         public IActionResult Create(NhanVien newItem = null)
         {
-            HttpContext.Session.SetString("NhanVienMoi", JsonConvert.SerializeObject(newItem));
-            return View();
+
+            byte[] json;
+            if (HttpContext.Session.TryGetValue("StaffList", out json)) {
+                DsNhanVien = JsonConvert.DeserializeObject<List<NhanVien>>(HttpContext.Session.GetString("StaffList"));
+                LastStaffId = JsonConvert.DeserializeObject<float>(HttpContext.Session.GetString("LastStaffId"));
+                DsNhanVien.Add(newItem);
+                LastStaffId += 1;
+                HttpContext.Session.SetString("LastStaffId", JsonConvert.SerializeObject(LastStaffId));
+                HttpContext.Session.SetString("StaffList", JsonConvert.SerializeObject(DsNhanVien));
+                
+            }
+            return Redirect("/staff/index");
         }
         [HttpGet]
-        public IActionResult Create(int Id = 0)
+        public IActionResult Create()
         {
+            LastStaffId = JsonConvert.DeserializeObject<float>(HttpContext.Session.GetString("LastStaffId"));
+            if (this.LastStaffId % 10 == 0) {
+                ViewBag.LastStaffId = "NV-" + (this.LastStaffId / 10000).ToString().Substring(2) + "0";
+            }
+            else {
+                ViewBag.LastStaffId = "NV-" + (this.LastStaffId / 10000).ToString().Substring(2);
+            }
+            
             return View();
         }
-        public IActionResult Edit()
+        [HttpPost]
+        public IActionResult Edit(NhanVien newItem = null)
         {
-            return Content("dang xay dung");
+            byte[] json;
+            if (HttpContext.Session.TryGetValue("StaffList", out json)) {
+                DsNhanVien = JsonConvert.DeserializeObject<List<NhanVien>>(HttpContext.Session.GetString("StaffList"));         
+            }
+            foreach (NhanVien nv in DsNhanVien) {
+                if (nv.maNhanVien == newItem.maNhanVien) {
+
+                }
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            byte[] json;
+            if (HttpContext.Session.TryGetValue("StaffList", out json)) {
+                DsNhanVien = JsonConvert.DeserializeObject<List<NhanVien>>(HttpContext.Session.GetString("StaffList"));
+            }
+            return View(GetByMNV(id));
         }
         public IActionResult Update()
         {
-            return Content("dang xay dung");
+            return View();
         }
         public IActionResult Delete()
         {
@@ -50,6 +89,29 @@ namespace MVCProject.Controllers
         public IActionResult Report()
         {
             return Content("dang xay dung");
+        }
+       public StaffController()
+        {
+            TaoDsNhanVien();
+            LastStaffId = 6;
+        }
+        public void TaoDsNhanVien()
+        {
+            DsNhanVien.Add(new NhanVien("NV-0001", "Đoàn Duy Quý", DateTime.Parse("28/6/2000"), "09715869331", "Ha Noi", "Nhan vien", 5));
+            DsNhanVien.Add(new NhanVien("NV-0002", "Nguyen Van A", DateTime.Parse("3/7/1999"), "09715869331", "Ha Noi", "Nhan vien", 5));
+            DsNhanVien.Add(new NhanVien("NV-0003", "Le Thi B", DateTime.Parse("8/12/1996"), "09715869331", "Ha Noi", "Nhan vien", 5));
+            DsNhanVien.Add(new NhanVien("NV-0004", "Trần Văn C", DateTime.Parse("28/6/2000"), "09715869331", "Ha Noi", "Nhan vien", 5));
+            DsNhanVien.Add(new NhanVien("NV-0005", "Bùi Tiến Đạt", DateTime.Parse("24/9/2000"), "09715869331", "Ha Noi", "Nhan vien", 5));
+        }
+        public NhanVien GetByMNV(string MNV)
+        {
+            NhanVien result = null;
+            DsNhanVien = JsonConvert.DeserializeObject<List<NhanVien>>(HttpContext.Session.GetString("StaffList"));
+            foreach(NhanVien nv in DsNhanVien) {
+                if (nv.maNhanVien == MNV) result = nv;
+            }
+
+            return result; 
         }
     }
 }
--- MVCProject/Models/NhanVien.cs
+++ MVCProject/Models/NhanVien.cs
@@ -8,14 +8,24 @@ namespace MVCProject.Models
 {
     public class NhanVien
     {
+        [Display(Name = "Mã Nhân Viên")]
+        [Required]
         public string maNhanVien { get; set; }
+        [Display(Name = "Họ Tên")]
+        [Required]
         public string hoTen { get; set; }
 
-       
+        [Display(Name = "Ngày Sinh")]
+        [Required]
         public DateTime ngaySinh { get; set; }
+        [Display(Name = "Số điện thoại")]
         public string soDienThoai { get; set; }
+        [Display(Name = "Địa Chỉ")]
         public string diaChi { get; set; }
+        [Display(Name = "Chức Vụ")]
+        [Required]
         public string chucVu { get; set; }
+        [Display(Name = "Số Năm Công Tác")]
         public int soNamCongTac { get; set; }
 
         public NhanVien() { }
