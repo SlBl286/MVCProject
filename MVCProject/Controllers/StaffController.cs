59d19d61fd397d80da86adadd8ea69787280539d
quy doan duy <quy.doanduy@gmail.com>
2021-03-16 13:46:21 +0700

    Add project files.

---
+++ MVCProject/Controllers/StaffController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Controllers
+{
+    public class StaffController : Controller
+    {
+        public List<NhanVien> DsNhanVien = new List<NhanVien> {
+            new NhanVien("NV-0001","Đoàn Duy Quý",DateTime.Parse("28/6/2000"),"09715869331","Ha Noi","Nhan vien",5),
+            new NhanVien("NV-0002","Nguyen Van A",DateTime.Parse("3/7/1999"),"09715869331","Ha Noi","Nhan vien",5),
+            new NhanVien("NV-0003","Le Thi B",DateTime.Parse("8/12/1996"),"09715869331","Ha Noi","Nhan vien",5),
+            new NhanVien("NV-0004","Trần Văn C",DateTime.Parse("28/6/2000"),"09715869331","Ha Noi","Nhan vien",5),
+            new NhanVien("NV-0005","Bùi Tiến Đạt",DateTime.Parse("24/9/2000"),"09715869331","Ha Noi","Nhan vien",5),
+        };
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("dsNhanVien", JsonConvert.SerializeObject(DsNhanVien));
+
+            return View(DsNhanVien);
+        }
+        [HttpPost]
+        public IActionResult Create(NhanVien newItem = null)
+        {
+            HttpContext.Session.SetString("NhanVienMoi", JsonConvert.SerializeObject(newItem));
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create(int Id = 0)
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return Content("dang xay dung");
+        }
+        public IActionResult Update()
+        {
+            return Content("dang xay dung");
+        }
+        public IActionResult Delete()
+        {
+            return Content("dang xay dung");
+        }
+        public IActionResult Report()
+        {
+            return Content("dang xay dung");
+        }
+    }
+}
---
+++ MVCProject/Models/NhanVien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Models
+{
+    public class NhanVien
+    {
+        public string maNhanVien { get; set; }
+        public string hoTen { get; set; }
+
+       
+        public DateTime ngaySinh { get; set; }
+        public string soDienThoai { get; set; }
+        public string diaChi { get; set; }
+        public string chucVu { get; set; }
+        public int soNamCongTac { get; set; }
+
+        public NhanVien() { }
+        public NhanVien(string maNhanVien, string hoTen, DateTime ngaySinh, string soDienThoai, string diaChi, string chucVu, int soNamCongTac)
+        {
+            this.maNhanVien = maNhanVien;
+            this.hoTen = hoTen;
+            this.ngaySinh = ngaySinh;
+            this.soDienThoai = soDienThoai;
+            this.diaChi = diaChi;
+            this.chucVu = chucVu;
+            this.soNamCongTac = soNamCongTac;
+        }
+    }
+}
